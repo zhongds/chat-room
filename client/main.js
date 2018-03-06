160a85c851e73f7effd645b43a7d78894af0fb4f
zhongds <zhongds@sutpc.com>
2018-03-06 17:59:59 +0800

    enhance static html

--- client/main.js
+++ client/main.js
@@ -1,13 +1,54 @@
 var loginEl = document.getElementById('login');
-var chatEl = document.getElementById('chat');
-var usernameEl = document.getElementsByClassName('username')[0]
+var homeEl = document.getElementById('home');
+var chatRoomEl = document.getElementById('chat-room');
 
-usernameEl.addEventListener('keydown', function(event) {
+var usernameEl = document.getElementsByClassName('username')[0];
+var displayNameEl = document.getElementById('displayName');
+
+// 展示聊天室名字
+var chatRoomHeader = document.querySelector('#chat-room .header');
+var chatRoomInput = document.getElementById('text-input');
+var gobackEl = document.getElementById('goback');
+var detailEl = document.getElementById('detail');
+
+usernameEl.focus();
+
+gobackEl.addEventListener('click', function(event) {
+  loginEl.classList.add('none');
+  chatRoomEl.classList.add('none');
+  homeEl.classList.remove('none');
+})
+
+var username;
+
+// usernameEl.addEventListener('keydown', function(event) {
+//   if(event.keyCode === 13) {
+//     username = usernameEl.value.trim();
+//     if(username) {
+//       loginEl.classList.add('none');
+//       chatRoomEl.classList.add('none');
+//       homeEl.classList.remove('none');
+//       displayNameEl.innerHTML = username;
+//     }
+//   }
+// })
+
+chatRoomInput.addEventListener('keydown', function(event) {
+  event.stopPropagation();
   if(event.keyCode === 13) {
-    var username = usernameEl.value.trim();
-    if(username) {
-      loginEl.classList.add('none');
-      chatEl.classList.remove('none');
+    var content = chatRoomInput.nodeValue.trim();
+    console.log('input content', content);
+    if(content) {
+      //跟后台交互，让它广播
     }
+  }
+})
+
+homeEl.addEventListener('click', function(event) {
+  console.log('event', event.target);
+  var targetEl = event.target;
+  if(targetEl.tagName === 'SPAN') {
+    var roomName = targetEl.getAttribute('name');
+    // 建立指定的socket连接，
   }
 })
